bbc5455f2e81b925922481c0d2715f2c1a32baa6
ATorada <sabiintg@gmail.com>
2022-12-13 17:51:01 +0100

    FEAT - Scroll thing deleted & now is responsive

--- prueba.js
+++ prueba.js
@@ -1,20 +1,20 @@
 //Al cargar la página mostrará durante 5 segundos el elementio de id heart y luego hará el main visible
 
-window.onload = function() {
-    setTimeout(function() {
+window.onload = function () {
+    setTimeout(function () {
         document.getElementById('heart').style.opacity = '0';
     }, 5000);
 
-    setTimeout(function() {
+    setTimeout(function () {
         document.getElementById('yoSoy').style.opacity = '1';
     }, 7000);
 
-    setTimeout(function() {
+    setTimeout(function () {
         document.getElementById('yoSoy').style.opacity = '0';
         document.querySelector('main').style.display = 'block';
     }, 10000);
 
-    setTimeout(function() {
+    setTimeout(function () {
         document.querySelector('main').style.opacity = '1';
     }, 14000);
 
